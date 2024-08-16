251c92f70eeeb39156e38b35d66acec26756cc6f
kamrulmit05 <kamrulmit05@gmail.com>
2024-08-16 21:04:57 +0600

    variable declaration format

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("Hello, ninjas!")
+
+	// Variable Decalaration types
+	var nameOne string = "Kamrul"
+	var nameTwo = "Jewel"
+	var nameThree string
+	nameThree = "Rakib"
+	nameFour := "Nizam"
+
+	fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+}
