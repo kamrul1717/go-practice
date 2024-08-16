7f8cdd6dcbf6e34aac83452820667dafa59d039d
kamrulmit05 <kamrulmit05@gmail.com>
2024-08-16 21:48:22 +0600

    print, formatting, specifiers

--- main.go
+++ main.go
@@ -3,14 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	// fmt.Println("Hello, ninjas!")
+	name := "kamrul"
+	age := 30
 
-	// Variable Decalaration types
-	var nameOne string = "Kamrul"
-	var nameTwo = "Jewel"
-	var nameThree string
-	nameThree = "Rakib"
-	nameFour := "Nizam"
+	// Print Function
+	fmt.Print("Hello, ")
+	fmt.Print("World!\n")
+	fmt.Print("new line\n")
 
-	fmt.Println(nameOne, nameTwo, nameThree, nameFour)
+	// Println Function
+	fmt.Println("Hello guys!")
+	fmt.Println("Goodbye guys!")
+	fmt.Println("my age is", age, "and my name is", name)
+
+	// Printf (formatted strings)
+	fmt.Printf("my age is %v and my name is %v\n", age, name)
+	fmt.Printf("my age is %q and my name is %q\n", age, name)
+	fmt.Printf("age is of type %T\n", age)
+	fmt.Printf("you scored %0.2f points\n", 225.275)
+
+	// Sprintf (save formatted strings)
+	var str = fmt.Sprintf("my age is %v and my name is %v\n", age, name)
+	fmt.Println("the saved string is", str)
+
 }
