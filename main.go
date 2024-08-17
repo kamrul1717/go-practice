d82c0cca325c993bfdd8dbe256e56f3e8cafc256
kamrulmit05 <kamrulmit05@gmail.com>
2024-08-17 15:30:19 +0600

    package scope

--- main.go
+++ main.go
@@ -1,30 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func getInitials(n string) (string, string) {
-	s := strings.ToUpper(n)
-	names := strings.Split(s, " ")
-
-	var initials []string
-	for _, v := range names {
-		initials = append(initials, v[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-	return initials[0], "_"
-}
+var scores = 99.5
 
 func main() {
-	firstName1, SecondName1 := getInitials("kamrul hasan")
-	fmt.Println(firstName1, SecondName1)
-	firstName2, SecondName2 := getInitials("rakib hasan")
-	fmt.Println(firstName2, SecondName2)
-	firstName3, SecondName3 := getInitials("nizam")
-	fmt.Println(firstName3, SecondName3)
+
+	sayHello("kamrul")
+
+	for _, v := range points {
+		fmt.Println(v)
+	}
+
+	showScore()
+
 }
