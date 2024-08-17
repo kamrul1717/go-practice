44619c389728e51729ce1f8a968beeef93041b55
kamrulmit05 <kamrulmit05@gmail.com>
2024-08-17 15:39:05 +0600

    maps

--- main.go
+++ main.go
@@ -2,16 +2,36 @@ package main
 
 import "fmt"
 
-var scores = 99.5
-
 func main() {
-
-	sayHello("kamrul")
-
-	for _, v := range points {
-		fmt.Println(v)
+	menu := map[string]float64{
+		"soup":           4.99,
+		"pie":            7.99,
+		"salad":          6.99,
+		"toffee pudding": 3.55,
 	}
 
-	showScore()
+	fmt.Println(menu)
+	fmt.Println(menu["pie"])
+
+	// looping maps
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// ints as key type
+	phonebook := map[int]string{
+		9595985: "kamrul",
+		959595:  "rakib",
+		95955:   "jewel",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[95955])
+
+	phonebook[9595985] = "nizam"
+	fmt.Println(phonebook)
+
+	phonebook[959595] = "mahid"
+	fmt.Println(phonebook)
 
 }
