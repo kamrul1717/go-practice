6ec6ba7c9fff653771af43c30a58edc0b783fbcc
kamrulmit05 <kamrulmit05@gmail.com>
2024-08-17 23:09:08 +0600

    pointer

--- main.go
+++ main.go
@@ -2,28 +2,22 @@ package main
 
 import "fmt"
 
-func updateName(x string) string {
-	x = "kamrul"
-	return x
-}
-
-func updateMenu(y map[string]float64) {
-	y["coffee"] = 2.99
+func updateName(x *string) {
+	*x = "kamrul"
 }
 
 func main() {
 	name := "rakib"
 
-	name = updateName(name)
+	//updateName(name)
 
+	fmt.Println("memory address of name is:", &name)
+
+	m := &name
+
+	fmt.Println("memory address of m is:", m)
+	fmt.Println("value at memory address:", *m)
+	updateName(m)
 	fmt.Println(name)
-
-	menu := map[string]float64{
-		"pie":       5.95,
-		"ice cream": 7.93,
-	}
-
-	updateMenu(menu)
-	fmt.Println(menu)
 
 }
