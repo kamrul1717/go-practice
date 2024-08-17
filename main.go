de0de9c0a1628dc742c03ddd994710997ec29fe0
kamrulmit05 <kamrulmit05@gmail.com>
2024-08-17 12:18:46 +0600

    conditionals and break, continue

--- main.go
+++ main.go
@@ -3,29 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	/*
-		x := 0
-		for x < 5 {
-			fmt.Println("the value of x is:", x)
-			x++
-		}
-	*/
+	age := 25
 
-	/*
-		for i := 0; i < 5; i++ {
-			fmt.Println("the value of i is:", i)
-		}
-	*/
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 50)
+	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 45")
+	}
 
 	names := []string{"kamrul", "rakib", "jewel", "nizam"}
-	// for i := 0; i < len(names); i++ {
-	// 	fmt.Println(names[i])
-	// }
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue
+		}
 
-	// for index, value := range names {
-	// 	fmt.Printf("the value at index %v is %v \n", index, value)
-	// }
-	for _, value := range names {
-		fmt.Printf("the value is %v \n", value)
+		if index > 2 {
+			fmt.Println("breaking at pos", index)
+			break
+		}
+
+		fmt.Printf("the value at pos %v is %v \n", index, value)
 	}
 }
