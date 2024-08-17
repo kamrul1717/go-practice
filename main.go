345c4d259bbc563ac934239f3e9203fb41e588e9
kamrulmit05 <kamrulmit05@gmail.com>
2024-08-17 23:27:22 +0600

    bill in progress

--- main.go
+++ main.go
@@ -2,22 +2,7 @@ package main
 
 import "fmt"
 
-func updateName(x *string) {
-	*x = "kamrul"
-}
-
 func main() {
-	name := "rakib"
-
-	//updateName(name)
-
-	fmt.Println("memory address of name is:", &name)
-
-	m := &name
-
-	fmt.Println("memory address of m is:", m)
-	fmt.Println("value at memory address:", *m)
-	updateName(m)
-	fmt.Println(name)
-
+	mybill := newBill("kamrul's bill")
+	fmt.Println(mybill.format())
 }
